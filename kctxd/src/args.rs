4d3153d817eecb2620833861b6e47f23baebbe4e
Paul Brousseau <object88@gmail.com>
2025-08-03 20:31:45 -0700

    chore: 3: implement log level flag on CLI (#18)

--- kctxd/src/args.rs
+++ kctxd/src/args.rs
@@ -1,17 +1,43 @@
-use clap::{arg, Command};
-use common::config::Config;
-use dirs::config_local_dir;
-use thiserror::Error;
-
 use std::path::PathBuf;
 
-pub struct Args{
+use clap::{arg, builder::{EnumValueParser, PossibleValue}, Arg, Command, ValueEnum};
+use common::config::Config;
+use dirs::config_local_dir;
+use log::LevelFilter;
+use thiserror::Error;
+
+/// ArgLevelFilter is a newtype for LevelFilter, so that ValueEnum can be
+/// implemented
+#[derive(Clone)]
+struct ArgLevelFilter(pub LevelFilter);
+
+// ValueEnum is necessary for EnumValueParser
+impl ValueEnum for ArgLevelFilter {
+  fn value_variants<'a>() -> &'a [Self] {
+    &[
+      ArgLevelFilter(LevelFilter::Off),
+      ArgLevelFilter(LevelFilter::Error),
+      ArgLevelFilter(LevelFilter::Warn),
+      ArgLevelFilter(LevelFilter::Info),
+      ArgLevelFilter(LevelFilter::Debug),
+      ArgLevelFilter(LevelFilter::Trace)
+    ]
+  }
+
+  fn to_possible_value(&self) -> Option<PossibleValue> {
+    Some(PossibleValue::new(Into::<LevelFilter>::into(self.0).to_string().to_lowercase()))
+  }
+}
+
+pub struct Args<'a>{
   c: Command,
+
+  cfg: &'a mut Config,
 
   // config_path: String
 }
 
-pub fn new(mut cfg: &Config) -> Result<Args, ArgsError> {
+pub fn new(cfg: &mut Config) -> Result<Args, ArgsError> {
   let default_config_dir: PathBuf = match config_local_dir() {
     Some(mut x) => {
       x.push("");
@@ -24,21 +50,36 @@ pub fn new(mut cfg: &Config) -> Result<Args, ArgsError> {
   };
 
   let default_config_dir: String = default_config_dir.to_string_lossy().to_string();
+  let default_log_level: String = cfg.server.log_level.to_string().to_lowercase();
 
   let cmd = Command::new(env!("CARGO_CRATE_NAME"))
     .long_version("...")
-    .arg(arg!(--config <FILE> "").default_value(default_config_dir));
+    .arg(arg!(--config <FILE> "").default_value(default_config_dir))
+    .arg(Arg::new("loglevel")
+      .long("log-level")
+      .required(false)
+      .default_value(default_log_level)
+      .help("Set the loglevel")
+      .long_help("Set the loglevel. 'trace' is the most verbose and 'off' the least verbose")
+      .value_parser(EnumValueParser::<ArgLevelFilter>::new())
+    );
+
   let a: Args = Args{
     c: cmd,
+    cfg,
     // config_path: default_config_dir,
   };
   Ok(a)
 }
 
-impl Args {
+impl Args<'_> {
   pub fn parse(self) {
     match self.c.get_matches() {
-        _ => {},
+      a => {
+        let log_level_value: &LevelFilter = &a.get_one::<ArgLevelFilter>("loglevel").unwrap().0;
+        
+        self.cfg.server.log_level = *log_level_value;
+      },
     }
   }
 }
